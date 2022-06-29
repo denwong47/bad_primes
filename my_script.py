06b728502a6f1753e69cca1a6ebed3ab51b8aa17
Denny Wong <dwong@forestreet.com>
2022-06-29 08:07:06 +0100

    Added timeit.

--- my_script.py
+++ my_script.py
@@ -1,3 +1,13 @@
+from tqdm import tqdm
+from timeit import timeit
+
+UPPER_LIMIT_TO_TEST = 1000000
+NUMBER_OF_PRIMES    = 78498
+
+# UPPER_LIMIT_TO_TEST = 10
+# NUMBER_OF_PRIMES    = 4
+
+
 def is_prime(c):
   for i in range(2, c):
     if (c % i == 0): return False
@@ -8,3 +18,11 @@ def list_primes(u):
     if (is_prime(i)):
       l.append(i)
   return l
+
+
+
+
+def _task():
+  assert len(list_primes(UPPER_LIMIT_TO_TEST)) == NUMBER_OF_PRIMES
+
+print (f"Calculating all primes up to {UPPER_LIMIT_TO_TEST:,} had taken {timeit(_task, number=1):,.6f}s.")
